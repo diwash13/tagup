9323fd57602cd202ee4c22a52a2a27794eb38101
Diwash Sigdel <diwashsigdel13@gmail.com>
2019-05-16 14:37:11 -0400

    Add Logs Component

--- src/Components/Logs/Logs.js
+++ src/Components/Logs/Logs.js
@@ -1,6 +1,20 @@
 import React from "react";
 import Log from "../Log/Log";
+import PropTypes from "prop-types";
 
-const Logs = props => props.logs.map(log => <Log key={log.id} log={log} />);
+const Logs = props =>
+  props.logs.map((log, index) => (
+    <Log key={log.id} log={log} deleteLog={() => props.deleteLog(index)} />
+  ));
+
+Logs.propTypes = {
+  deleteLog: PropTypes.func.isRequired,
+  log: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    message: PropTypes.string.isRequired,
+    timestamp: PropTypes.string.isRequired
+  })
+};
 
 export default Logs;
