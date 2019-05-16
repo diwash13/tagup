5e6385f248975323847ea37964c96b1ab1e27725
Diwash Sigdel <diwashsigdel13@gmail.com>
2019-05-16 14:40:06 -0400

    Add Logs to state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
 import React, { Component } from "react";
 import LogForm from "./Components/LogForm/LogForm";
 import { data } from "./Data";
-import Logs from './Components/Logs/Logs'
+import Logs from "./Components/Logs/Logs";
+import Seacrh from "./Components/Search/Search";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      logs: [...data]
+      logs: [...data],
+      searchText: ""
     };
   }
 
-  addLog = (log) => {
-    const logWithId = {...log, id:this.state.logs.length}
-    this.setState({logs:[logWithId, ...this.state.logs]})
-  //   const newLogArr = this.state.logs.slice().push(log)
-  //   this.setState({logs: newLogArr})
-  }
+  addLog = log => {
+    const logWithId = { ...log, id: Date.now() };
+    this.setState({ logs: [logWithId, ...this.state.logs] });
+    //   const newLogArr = this.state.logs.slice().push(log)
+    //   this.setState({logs: newLogArr})
+  };
+
+  deleteLog = index => {
+    const newLogs = this.state.logs.slice();
+    newLogs.splice(index, 1);
+    this.setState({ logs: newLogs });
+  };
+
+  handleSearchChange = e => {
+    this.setState({
+      searchText: e.target.value
+    });
+  };
+
+  filteredLog = () => {
+    return this.state.logs.filter(log =>
+      log.title.indexOf(this.state.searchText) === -1 ? false : true
+    );
+  };
 
   render() {
-    console.log(this.state)
     return (
       <main className="App">
-        <LogForm addLog={this.addLog}/>
-        <Logs logs={this.state.logs}/>
+        <LogForm addLog={this.addLog} />
+        <Seacrh onChange={this.handleSearchChange} />
+        <Logs logs={this.filteredLog()} deleteLog={this.deleteLog} />
       </main>
     );
   }
