053af30faabf653896f2ff755dd400166012fc9e
Diwash Sigdel <diwashsigdel13@gmail.com>
2019-05-16 14:36:44 -0400

    Add Log Component

--- src/Components/LogForm/LogForm.js
+++ src/Components/LogForm/LogForm.js
@@ -1,58 +1,75 @@
-import React, { Component } from 'react';
-import Card from '../Card/Card'
+import React, { Component } from "react";
+import Card from "../Card/Card";
+import Button from "../Button/Button";
+import styles from "./logform.module.css";
 
 class LogForm extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            title: '',
-            message: ''
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      message: ""
+    };
+  }
 
-    addLog = (e) => {
-        e.preventDefault()
-        this.props.addLog({
-            ...this.state,
-            timestamp: new Date().toISOString()
-        })
-    }
+  addLog = e => {
+    e.preventDefault();
+    this.props.addLog({
+      ...this.state,
+      timestamp: new Date().toISOString()
+    });
+    this.clearState();
+  };
 
-    handleChangeTitle = (val) => {
-        console.log(val)
-        this.setState({
-          title: val
-        })
-      }
+  clearState = () => {
+    this.setState({
+      title: "",
+      message: ""
+    });
+  };
 
-      handleChangeMessage = (val) => {
-        console.log(val)
-        this.setState({
-          message: val
-        })
-      }
+  handleChangeTitle = val => {
+    this.setState({
+      title: val
+    });
+  };
 
-    render() {
-        return (
-            <Card> 
-                <h3>Add New Log</h3>
-                <form onSubmit={this.addLog}>
-                <input 
-                    type="text" 
-                    placeholder='Log Title' 
-                    onChange={e => this.handleChangeTitle(e.target.value)}
-                    value={this.state.title}/>
-                <br/>
-                <textarea name="Log Message" placeholder='Log Message' cols="30" rows="10" 
-                onChange={e => this.handleChangeMessage(e.target.value)}
-                value={this.state.message}
-                ></textarea>
-                <br/>
-                <button type='submit'>Submit Log</button>
-                </form>
-            </Card>
-        )
-    }
+  handleChangeMessage = val => {
+    this.setState({
+      message: val
+    });
+  };
+
+  render() {
+    return (
+      <Card>
+        <h3>Add New Log</h3>
+        <form onSubmit={this.addLog}>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Log Title"
+            onChange={e => this.handleChangeTitle(e.target.value)}
+            value={this.state.title}
+          />
+          <textarea
+            className={styles.textarea}
+            name="Log Message"
+            placeholder="Log Message"
+            cols="30"
+            rows="10"
+            onChange={e => this.handleChangeMessage(e.target.value)}
+            value={this.state.message}
+          />
+          <Button
+            className="far fa-comment-alt"
+            type="submit"
+            label={" Submit"}
+          />
+        </form>
+      </Card>
+    );
+  }
 }
 
-export default LogForm
+export default LogForm;
